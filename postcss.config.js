6fdecec5d68a3e277f5ddb6252efda39e02ece75
Katsushi OUGI <ougi@to-r.net>
2023-11-10 14:50:05 +0900

    minify

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,20 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+console.log("isProduction", process.env.NODE_ENV);
+
 module.exports = {
   map: { inline: false },
   plugins: [
     require("postcss-sort-media-queries")({
       sort: "mobile-first",
     }),
-  ],
+  ].concat(
+    isProduction
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : [],
+  ),
 };
